da26fa484d0d9d6672bd7e12e8a51f3bbaf5e4a5
Alberto <alber1183@hotmail.com>
2016-03-28 18:29:25 +0200

    Ficheros

---
+++ FolderSize/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Carpeta = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Tamaño = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(550, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Leer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Carpeta,
+            this.Tamaño,
+            this.Size});
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 35);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(736, 422);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // Carpeta
+            // 
+            this.Carpeta.Tag = "Text";
+            this.Carpeta.Text = "Carpeta";
+            this.Carpeta.Width = 471;
+            // 
+            // Tamaño
+            // 
+            this.Tamaño.Tag = "Text";
+            this.Tamaño.Text = "Tamaño";
+            this.Tamaño.Width = 142;
+            // 
+            // Size
+            // 
+            this.Size.Tag = "Numeric";
+            this.Size.Text = "Size";
+            this.Size.Width = 88;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(531, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(625, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 469);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tamaño de Carpetas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ColumnHeader Carpeta;
+        public System.Windows.Forms.ColumnHeader Tamaño;
+        public System.Windows.Forms.ColumnHeader Size;
+    }
+}
+
---
+++ FolderSize/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Globalization;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+       //private int sortColumn = -1;
+        public Form1()
+        {
+            InitializeComponent(); 
+            listView1.ListViewItemSorter = new Sorter();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sizeOfDir_total = 0;
+            listView1.Items.Clear();
+            foreach (string dir in Directory.GetDirectories(textBox1.Text))
+            {
+                try
+                {
+                    DirectoryInfo dInfo = new DirectoryInfo(@dir);
+                    // set bool parameter to false if you
+                    // do not want to include subdirectories.
+                    double sizeOfDir = DirectorySize(dInfo, true);
+                    sizeOfDir_total += sizeOfDir;
+
+                    String size = "";
+
+                    if (sizeOfDir < 1024)
+                    {
+                        size = sizeOfDir.ToESString() + " Bytes";
+                    }
+                    else if (sizeOfDir < 1024 * 1024)
+                    {
+                        size = Math.Round(sizeOfDir / 1024, 3).ToESString() + " KB";
+                    }
+                    else if (sizeOfDir < 1024 * 1024 * 1024)
+                    {
+                        size = Math.Round(sizeOfDir / (1024 * 1024), 3).ToESString() + " MB";
+                    }
+                    else
+                    {
+                        size = Math.Round(sizeOfDir / (1024 * 1024 * 1024), 3).ToESString() + " GB";
+                    }
+
+                    ListViewItem objListViewItem = new ListViewItem();
+                    objListViewItem.Text = dir;
+                    objListViewItem.SubItems.Add(size);
+                    objListViewItem.SubItems.Add(sizeOfDir.ToESString());
+                    listView1.Items.Add(objListViewItem);
+
+                    /*Console.WriteLine("Directory size in Bytes : " +
+                    "{0:N0} Bytes", sizeOfDir);
+                    Console.WriteLine("Directory size in KB : " +
+                    "{0:N2} KB", ((double)sizeOfDir) / 1024);
+                    Console.WriteLine("Directory size in MB : " +
+                    "{0:N2} MB", ((double)sizeOfDir) / (1024 * 1024));
+
+                    Console.ReadLine();*/
+                }
+                catch
+                {
+
+                }
+            }
+
+            label1.Text = "Total: " + Math.Round(((double)sizeOfDir_total) / (1024 * 1024 * 1024), 3) + " GB";
+        }
+                
+        static long DirectorySize(DirectoryInfo dInfo, bool includeSubDir)
+        {
+            // Enumerate all the files
+            long totalSize = dInfo.EnumerateFiles()
+                         .Sum(file => file.Length);
+
+            // If Subdirectories are to be included
+            if (includeSubDir)
+            {
+                // Enumerate all sub-directories
+                totalSize += dInfo.EnumerateDirectories()
+                         .Sum(dir => DirectorySize(dir, true));
+            }
+            return totalSize;
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            Sorter s = (Sorter)listView1.ListViewItemSorter;
+            s.Column = e.Column;
+
+            if (s.Order == System.Windows.Forms.SortOrder.Ascending)
+            {
+                s.Order = System.Windows.Forms.SortOrder.Descending;
+            }
+            else
+            {
+                s.Order = System.Windows.Forms.SortOrder.Ascending;
+            }
+            listView1.Sort();
+        }
+
+        class Sorter : System.Collections.IComparer
+        {
+            public int Column = 1;
+            public System.Windows.Forms.SortOrder Order = SortOrder.Ascending;
+            public int Compare(object x, object y) // IComparer Member
+            {
+                if (!(x is ListViewItem))
+                    return (0);
+                if (!(y is ListViewItem))
+                    return (0);
+
+                ListViewItem l1 = (ListViewItem)x;
+                ListViewItem l2 = (ListViewItem)y;
+
+                if (l1.ListView.Columns[Column].Tag == null)
+                {
+                    l1.ListView.Columns[Column].Tag = "Text";
+                }
+
+                if (l1.ListView.Columns[Column].Tag.ToString() == "Numeric")
+                {
+                    double fl1 = double.Parse(l1.SubItems[Column].Text);
+                    double fl2 = double.Parse(l2.SubItems[Column].Text);
+
+                    if (Order == SortOrder.Ascending)
+                    {
+                        return fl1.CompareTo(fl2);
+                    }
+                    else
+                    {
+                        return fl2.CompareTo(fl1);
+                    }
+                }
+                else
+                {
+                    string str1 = l1.SubItems[Column].Text;
+                    string str2 = l2.SubItems[Column].Text;
+
+                    if (Order == SortOrder.Ascending)
+                    {
+                        return str1.CompareTo(str2);
+                    }
+                    else
+                    {
+                        return str2.CompareTo(str1);
+                    }
+                }
+            }
+        }
+    }
+
+    public static class DoubleExtensions
+    {
+        public static string ToGBString(this double value)
+        {
+            return value.ToString(CultureInfo.GetCultureInfo("en-GB"));
+        }
+        public static string ToESString(this double value)
+        {
+            return value.ToString(CultureInfo.GetCultureInfo("es-ES"));
+        }
+    }
+}
